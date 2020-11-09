78315e9e833ab7687e65883b12116e4fbd59a2eb
Victor Greiveldinger <VGrei@MacBook-Pro-de-Victor-3.local>
2020-11-09 13:45:36 +0100

    axios request

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,5 +1,6 @@
 // == Import
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 // Composants
 import Header from 'src/components/Header';
@@ -9,18 +10,35 @@ import Footer from 'src/components/Footer';
 
 // data, styles et utilitaires
 import categoriesData from 'src/data/categories';
-import postsData from 'src/data/posts';
+// Ici on veut récuperer les données de l'API et non plus celles du fichier data.
+// import postsData from 'src/data/posts';
 import { getPostsByCategory } from 'src/selectors/posts';
 import './styles.scss';
 
 // == Composant
 const Blog = () => {
+  console.log('Etat initial');
+  const [posts, setPosts] = useState([]);
+
+  useEffect (
+    () => {
+      console.log('plop');
+      axios.get('https://oclock-open-apis.now.sh/api/blog/posts')
+        .then((response) => {
+          console.log(response);
+          setPosts(response.data);
+        })
+    },
+    [],
+  );
+
+  console.log('rendu');
   return (
     <div className="blog">
       <Header categories={categoriesData} />
         {categoriesData.map((category) => (
             <Posts
-              posts={getPostsByCategory(postsData, category.label)}
+              posts={getPostsByCategory(posts, category.label)}
               category={category.label}
             />
           ))}
@@ -28,6 +46,15 @@ const Blog = () => {
     </div>
   );
 };
+
+
+/* TEST AXIOS
+
+*/
+
+// let promise = axios.get('https://oclock-open-apis.now.sh/api/blog/posts');
+// console.log(promise);
+// promise.then((response) => { console.log(promise);});
 
 // == Export
 export default Blog;
